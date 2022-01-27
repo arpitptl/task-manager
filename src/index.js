f3986398e2154948065daf968d78a3fe802ae558
Arpit Patel <arpit@awaaz.de>
2022-01-27 15:38:48 +0530

    create env file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 
 app.listen(port, () => {
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,9 +4,7 @@ const jwt = require('jsonwebtoken')
 const auth = async function (req, res, next) {
     try {
         const token = req.header('Authorization').replace('Bearer', '').trim()
-        // const t = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWYwZjZjZjY0MWI0NzlmY2NkMzNhMzgiLCJpYXQiOjE2NDMxODE3NzV9.6LBkgLwt58KYIIHS71k3643Vn2aVik5GKGeB1Om9PfM'
-        const decoded = jwt.verify(token, 'jsonwebtoken')
-        console.log(decoded)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findOne({
             _id: decoded._id,
             'tokens.token': token
